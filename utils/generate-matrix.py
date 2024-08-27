c17d3f47927d839a4a1e523712844541e507f66f
Mickael BARON <baron@ensma.fr>
2024-08-27 18:49:10 +0200

    Update script

--- utils/generate-matrix.py
+++ utils/generate-matrix.py
@@ -4,8 +4,7 @@ import argparse
 from datetime import datetime
 
 
-def generate_matrix(versions, input_image, output_image, spark_version,
-                    gpu_options, version_prefix):
+def generate_matrix(versions, input_image, output_image, gpu_options, version_prefix):
     """
     Generate the build matrix for the given versions and options.
 
@@ -13,7 +12,6 @@ def generate_matrix(versions, input_image, output_image, spark_version,
         versions (list): A list of version strings (Python or R versions).
         input_image (str): The base image name.
         output_image (str): The output image name.
-        spark_version (str): The Spark version to include in the output image tag.
         gpu_options (list): A list of booleans indicating whether to build GPU-enabled images.
         version_prefix (str): A prefix to denote the language version ("py" or "r").
 
@@ -24,16 +22,10 @@ def generate_matrix(versions, input_image, output_image, spark_version,
     for version in versions:
         base = f"{input_image}:latest" if input_image == "base" else f"{input_image}:{version_prefix}{version}"
         output = f"{output_image}:{version_prefix}{version}"
-        language_key = "python_version" if version_prefix == "py" else "r_version"
+        language_key = "java_version" if version_prefix == "py" else "r_version"
         version_entry = {"base_image_tag": f"{DH_ORGA}/{args.images_prefix}-{base}",
                          "output_image_main_tag": f"{DH_ORGA}/{args.images_prefix}-{output}",
                          language_key: version}
-        if spark_version:
-            suffix_spark = f"-spark{spark_version}"
-            if "spark" in base:
-                version_entry["base_image_tag"] += suffix_spark
-            version_entry["output_image_main_tag"] += suffix_spark
-            version_entry["spark_version"] = spark_version
         for gpu in gpu_options:
             final_entry = version_entry.copy()
             suffix_gpu = "-gpu" if gpu else ""
@@ -49,20 +41,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_image", type=str)
     parser.add_argument("--output_image", type=str)
-    parser.add_argument("--python_version_1", type=str, nargs="?", const="")
-    parser.add_argument("--python_version_2", type=str, nargs="?", const="")
-    parser.add_argument("--r_version_1", type=str, nargs="?", const="")
-    parser.add_argument("--r_version_2", type=str, nargs="?", const="")
-    parser.add_argument("--spark_version", type=str, nargs="?", const="")
+    parser.add_argument("--java_version_1", type=str, nargs="?", const="")
+    parser.add_argument("--javav_version_2", type=str, nargs="?", const="")
+    parser.add_argument("--javav_version_3", type=str, nargs="?", const="")
     parser.add_argument("--build_gpu", type=str, nargs="?")
     parser.add_argument("--base_image_gpu", type=str, nargs="?", const="")
     parser.add_argument("--dh_orga", type=str)
     parser.add_argument("--images_prefix", type=str)
 
     args = parser.parse_args()
-    python_versions = [version for version in [args.python_version_1, args.python_version_2]
+    java_versions = [version for version in [args.java_version_1, args.java_version_2, args.java_version_3]
                        if version]
-    r_versions = [version for version in [args.r_version_1, args.r_version_2] if version]
     gpu_options = [False, True] if args.build_gpu == "true" else [False]
 
     DH_ORGA = args.dh_orga.lower()
@@ -85,12 +74,9 @@ if __name__ == "__main__":
             ]
     else:
         # Subsequent images, with versioning
-        if python_versions:
-            matrix = generate_matrix(python_versions, args.input_image, args.output_image,
-                                     args.spark_version, gpu_options, "py")
-        elif r_versions:
-            matrix = generate_matrix(r_versions, args.input_image, args.output_image,
-                                     args.spark_version, gpu_options, "r")
+        if java_versions:
+            matrix = generate_matrix(java_versions, args.input_image, args.output_image,
+                                     gpu_options, "py")
 
     matrix_json = json.dumps(matrix)
     print(matrix_json)
